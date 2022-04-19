2f6aa367f1e96784062986fa55d508e042cdabd5
absentmindedengineer <absentmindedengineer@gmail.com>
2022-04-19 20:46:55 +0200

    pilot part 2 with aim.

--- 2021/day2/pilot_aim.py
+++ 2021/day2/pilot_aim.py
@@ -15,20 +15,20 @@ with open('day2/cmds.txt','r') as f:
     lines = f.readlines()
     for element in lines:
         converted_list.append(element.strip())
-        #string elements consisting of a string and an integer.
-
+        
 horizontal_length=0
 vertical_depth=0
-
+aim=0
 for i in range(len(converted_list)):
     str_element = converted_list[i]
     if(str_element.find('forward') != -1):
         forward_length = int(re.search(r'\d+', str_element).group())
         horizontal_length +=forward_length
+        vertical_depth+=aim*forward_length
     elif (str_element.find('down') != -1):
-        vertical_depth+=int(re.search(r'\d+', str_element).group())
+        aim+=int(re.search(r'\d+', str_element).group())
     elif (str_element.find('up') != -1):
-        vertical_depth-=int(re.search(r'\d+', str_element).group())
+        aim-=int(re.search(r'\d+', str_element).group())
 
 print(f"horizontal length: {horizontal_length}, depth = {vertical_depth}")
 print(f"multiplied: {horizontal_length*vertical_depth}")
