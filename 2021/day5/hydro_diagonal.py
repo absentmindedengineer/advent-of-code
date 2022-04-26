dbe2bfee95d0098699ed9e5936185a13413b5332
OskarComp <absentmindedengineer@gmail.com>
2022-04-27 01:07:18 +0200

    Part2. Had a realization I wasn't allowed to be as careless as to start at the minimum value as in the vertical and horizontal case, which made it a lot easier to miss the difficulties when adding the diagonal lines. Initially I was simply using the same range function with the minimum values which cause you to take the incorrect lines quite frequently. Additionally it doesn't complain if you make a range(5,3) and will just be a silent error. Additionally you of course need to go from point 1 to point 2, but this causes you to take care of which value is the greatest to correctly create your ranges and therefore using the correct lines (or any for that matter).

--- 2021/day5/hydro_diagonal.py
+++ 2021/day5/hydro_diagonal.py
@@ -43,14 +43,15 @@ for idx in range(len(tmp_lst)):
         #horizontal
         line_data.append(tmp_lst[idx])
         line_bool_data.append('horizontal')
-    # elif abs(tmp_lst[idx][0]-tmp_lst[idx][2]) == abs(tmp_lst[idx][1]-tmp_lst[idx][3]):
-    #     line_data.append(tmp_lst[idx])
-    #     line_bool_data.append('diagonal')
+    elif abs(tmp_lst[idx][0]-tmp_lst[idx][2]) == abs(tmp_lst[idx][1]-tmp_lst[idx][3]):
+        #diagonal
+        line_data.append(tmp_lst[idx])
+        line_bool_data.append('diagonal')
     else:
         pass
         #not horizontal or vertical.
 
-field = np.zeros((1000,1000), dtype=int)
+field_diag = np.zeros((1000,1000), dtype=int)
 
 for line in range(len(line_data)):
     # Since they are horizontal or vertical it's just as good to decide which it is.
@@ -59,33 +60,42 @@ for line in range(len(line_data)):
         #range is so fucking stupid.
         y_lst = sorted([int(line_data[line][1]), int(line_data[line][3])])
         for y_pos in range(y_lst[0],y_lst[1]+1): 
-            field[x_pos, y_pos]+=1
+            field_diag[x_pos, y_pos]+=1
     elif line_bool_data[line] == 'horizontal':
         y_pos = int(line_data[line][1])
         x_lst = sorted([int(line_data[line][0]), int(line_data[line][2])])
         for x_pos in range(x_lst[0],x_lst[1]+1):
-            field[x_pos, y_pos]+=1
-    # elif line_bool_data[line] == 'diagonal':
-    #     x_lst = sorted([int(line_data[line][0]), int(line_data[line][2])])
-    #     x_range = range(x_lst[0],x_lst[1]+1)
-    #     y_lst = sorted([int(line_data[line][1]), int(line_data[line][3])])
-    #     y_range = range(y_lst[0],y_lst[1]+1)
-    
-        #if it's diagonal it has the same length of x and y. this means we only
-        #need one loop.
-        # for idx in range(len(x_range)):
-        #     x_pos = x_range[idx]
-        #     y_pos = y_range[idx]
-        #     field[x_pos,y_pos] +=1
+            field_diag[x_pos, y_pos]+=1
+    elif line_bool_data[line] == 'diagonal':
+        #take the min_x, use the corresponding y-value
+        #and find out if u wanna decrease the y-value or not.
+        if line_data[line][0] < line_data[line][2]:
+            x_range = range(line_data[line][0], line_data[line][2]+1)
+        else:
+            x_range = range(line_data[line][0],line_data[line][2]-1,-1)
+        if line_data[line][1] < line_data[line][3]:
+            y_range = range(line_data[line][1],line_data[line][3]+1)
+        else:
+            #assuming no zero length stuff.
+            y_range = range(line_data[line][1],line_data[line][3]-1,-1)
+        
+        for idx in range(len(x_range)):
+            #if it's diagonal it has the same length of x and y. this means we only
+            #need one loop.
+            x_pos = x_range[idx]
+            y_pos = y_range[idx]
+            field_diag[x_pos,y_pos] +=1
+
+
     else:
         print("logic error")
 
-print(field[np.where(field >= 2)].shape)
+print(field_diag[np.where(field_diag >= 2)].shape)
 
 counter = 0
 
 for x in range(1000):
     for y in range(1000):
-        if field[x,y] > 1:
+        if field_diag[x,y] > 1:
             counter+=1
 print(counter)
