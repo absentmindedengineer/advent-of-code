d40b37b7c607dd2ea8ef4d8169f746420b5585d4
OskarComp <absentmindedengineer@gmail.com>
2022-04-28 00:19:13 +0200

    At first troubled by how one had different wait time for birth, not an easy 1-fish factorial/7days or so. But given the idea of counting the number fish based on this very predictable pattern and understanding the inevitable problem by handling each fish independently as if they were all different.

---
+++ 2021/day6/immortal_lantern_fish.py
@@ -0,0 +1,23 @@
+fish_lst = []
+with open('day6/initial_fish_state.txt','r') as f:
+    line = f.readline()
+    tmp_str_lst = list(line.split(','))
+for i in range(len(tmp_str_lst)):
+    fish_lst.append(int(tmp_str_lst[i]))
+
+fish_num = [0]*9
+for i in range(len(fish_lst)):
+    fish_num[fish_lst[i]] +=1 
+
+
+for i in range(1,257):
+    
+    fish_num_tmp = [0]*9
+    fish_num_tmp[0:8] = fish_num[1:9]
+    fish_num_tmp[8] = fish_num[0]
+    fish_num_tmp[6] += fish_num[0]
+    fish_num = fish_num_tmp
+
+    #print(f'day: {i} : {fish_num}, with sum {sum(fish_num)}')
+
+print(f"Amount of lantern fish after 256 days: {sum(fish_num)}")
