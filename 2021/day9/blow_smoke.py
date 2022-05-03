749fc1ff58d65568ada259f662e619b6350e9e4d
Acer Racer <absentmindedengineer@gmail.com>
2022-05-04 00:33:32 +0200

    Simple puzzle with a nice integer conversion for a long single list when appending to a 2d list. Additionally range a list of k items has a length of k, but a range of [0,k-1] which means an index will not be equal to the length but k-1 and messes up your if statements.

---
+++ 2021/day9/blow_smoke.py
@@ -0,0 +1,27 @@
+lst_2d_height = []
+with open('day9/height_map.txt','r') as f:
+    tmp_h = f.readlines()
+    for element in tmp_h:
+        lst_2d_height.append([int(elem) for elem in list(element.strip())])
+
+low_point_heights = []
+for idx_v in range(len(lst_2d_height)):
+    for idx_h in range(len(lst_2d_height[0])):
+        above,below,left,right = 10,10,10,10
+        # outside this map is set to higher than max val.
+        # idx vertical and horizontal
+        # look left, right, up or below.
+        point_height = lst_2d_height[idx_v][idx_h]
+        if idx_v != 0:
+            above = lst_2d_height[idx_v-1][idx_h]
+        if idx_v != len(lst_2d_height)-1:
+            below = lst_2d_height[idx_v+1][idx_h]
+        if idx_h != 0:
+            left = lst_2d_height[idx_v][idx_h-1]
+        if idx_h != len(lst_2d_height[0])-1:
+            right = lst_2d_height[idx_v][idx_h+1]
+        
+        if point_height < above and point_height < below and point_height < left and point_height < right:
+            low_point_heights.append(point_height) 
+
+print(f"risk level: {sum(low_point_heights)+len(low_point_heights)}")
