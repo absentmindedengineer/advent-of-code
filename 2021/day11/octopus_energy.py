6559a17aa1d2d3e626f7ed94dbe7dd1fdf673de6
OskarComp <absentmindedengineer@gmail.com>
2022-05-18 00:28:45 +0200

    space handling is started, but how the index_to_address array is initiated and concatenated based on if above,below,left,right is allowed isn't finished yet. I thought I could initiate it as something empty but it seems more problematic than just setting the first indices (to add) to something we will either remove or ignore later in the "for position in index_to_address".

--- 2021/day11/octopus_energy.py
+++ 2021/day11/octopus_energy.py
@@ -1,3 +1,5 @@
+import numpy as np
+import copy
 #10x10 grid of octopi with their own energy level.
 
 
@@ -14,3 +16,60 @@
         #a function to take care of an octopus at certain coordinates would likely be appropriate as one will cause flashes within flashes and would want to do that recursively.
 
 steps = 100
+energy_grid = np.full((5,5),None)
+raw_lst = []
+with open('day11/example_ocotpus.txt','r') as f:
+    raw_data = f.readlines()
+    for row_idx in range(len(raw_data)):
+        number_string = raw_data[row_idx].strip()
+        for idx in range(len(number_string)):
+            energy_grid[row_idx][idx] = int(number_string[idx])
+
+# indices to increase
+# [-1].-ö
+
+#index_to_address = np.array([[-1,-1],[-1,0],[-1,1],[0,-1],[0,1], [1,-1],[1,0],[1,1]])
+
+def flash_lightning(energy_grid, row_idx, column_idx, step_flashed_grid):
+    step_flashed_grid[row_idx,column_idx] = True
+    energy_grid[row_idx][column_idx] = 0
+
+
+    #space handling
+    index_to_address = np.array([[5,5]])#initial
+    if row_idx != 0 and column_idx != 0:
+        index_to_address = np.concatenate((index_to_address, np.array([[-1,-1],[-1,0]])))
+        #index_to_address = np.concatenate((index_to_address, np.array([-1,0])))
+    if row_idx != 0 and column_idx != len(energy_grid[row_idx]):
+        index_to_address = np.concatenate((index_to_address, np.array([[-1,1]])))
+    if column_idx != 0:
+        index_to_address = np.concatenate((index_to_address, np.array([[0,-1]])))
+    if column_idx != len(energy_grid[row_idx]):
+        index_to_address = np.concatenate((index_to_address, np.array([[0,1]])))
+    if row_idx != len(energy_grid) and column_idx != 0:
+        index_to_address = np.concatenate((index_to_address, np.array([[1,-1]])))
+        index_to_address = np.concatenate((index_to_address, np.array([[1,0]])))
+    if row_idx != len(energy_grid) and column_idx != len(energy_grid[row_idx]):
+        index_to_address = np.concatenate((index_to_address, np.array([[1,1]])))
+    
+
+    for position in index_to_address:
+        if step_flashed_grid == False:
+            energy_grid[row_idx+position[0]][column_idx+position[1]] += 1
+            if energy_grid[row_idx+position[0]][column_idx+position[1]] > 9:
+                flash_lightning(energy_grid, row_idx+position[0], column_idx+position[1], step_flashed_grid)
+        else:
+            #it did flash.
+            pass
+#initial_energy_grid = copy.copy(energy_grid)
+for step in range(1,steps):
+    step_flashed_grid = np.full((5,5),False) #only flash once so it resets every step.
+    for row_idx in range(len(energy_grid)):
+        for column_idx in range(len(energy_grid[row_idx])):
+            energy_grid[row_idx][column_idx] += 1
+            if energy_grid[row_idx][column_idx] > 9:
+                #flash-functionality.
+                flash_lightning(energy_grid, row_idx, column_idx, step_flashed_grid)
+            else:
+                #nothing
+                pass
